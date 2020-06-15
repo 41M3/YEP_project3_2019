cb2a63444784869e8486b63e1807608daea3349d
Gaëtan <gaetan.chaugny@epitech.eu>
2020-06-15 22:26:26 +0200

    [+]connect.js & connexion.ejs -> adding login functions and include connect.js

--- Web/public/js/connect.js
+++ Web/public/js/connect.js
@@ -1,44 +1,3 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAuCVJBCHJmtISIZ8FbzkrdFfAvY5aXEuI",
-    authDomain: "epiflipboard-3d596.firebaseapp.com",
-    databaseURL: "https://epiflipboard-3d596.firebaseio.com/",
-    projectId: "epiflipboard-3d596",
-    storageBucket: "epiflipboard-3d596.appspot.com",
-    messagingSenderId: "487209646537",
-    appId: "1:487209646537:web:864903f9fd9715efee9018",
-    measurementId: "G-ELXDR9YFCH"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-
-
-function LoginButtonPressed() {
-  window.alert("PUUUUTE");
-}
-
-/*
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -64,25 +23,23 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-function login(){
+function LoginButtonPressed() {
+  
+  var userEmail = document.getElementById("mail").value;
+  var userPass = document.getElementById("password").value;
 
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
+  window.alert(userEmail);
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
+    // Errors
     var errorCode = error.code;
     var errorMessage = error.message;
 
     window.alert("Error : " + errorMessage);
 
-    // ...
   });
-
 }
 
 function logout(){
   firebase.auth().signOut();
 }
-
-*/
