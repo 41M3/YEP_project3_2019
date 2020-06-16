d639e6ff6bfc506fb96844291d33b5b91bfebf92
Gaëtan <gaetan.chaugny@epitech.eu>
2020-06-16 20:53:38 +0200

    [+]inscription.js -> handle signUp function with email and password, + emailverification ->error 404 jesépapourkwa

---
+++ Web/public/js/inscription.js
@@ -0,0 +1,33 @@
+function handleSignUp() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    console.log(email);
+    console.log(password);
+
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+}
+
+function sendEmailVerification() {
+    firebase.auth().currentUser.sendEmailVerification().then(function() {
+      alert('Email Verification Sent!');
+    });
+}
