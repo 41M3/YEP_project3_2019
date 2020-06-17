42d29a09867f6b44244391b4aaa38fa95f5f0a5d
Gaëtan <gaetan.chaugny@epitech.eu>
2020-06-17 13:38:37 +0200

    [+]insciption.js/ejs -> error 404 fixed, still firebase.auth is not a function error

--- Web/public/js/inscription.js
+++ Web/public/js/inscription.js
@@ -1,3 +1,6 @@
+//API web key = AIzaSyA7ag14QpQ3BcEIDStib4OXcgoEQsQRPCI
+
+/*
 function handleSignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -32,3 +35,50 @@ function sendEmailVerification() {
       alert('Email Verification Sent!');
     });
 }
+
+handleSignUp();
+
+*/
+
+
+(function() {
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyAuCVJBCHJmtISIZ8FbzkrdFfAvY5aXEuI",
+    authDomain: "epiflipboard-3d596.firebaseapp.com",
+    databaseURL: "https://epiflipboard-3d596.firebaseio.com",
+    projectId: "epiflipboard-3d596",
+    storageBucket: "epiflipboard-3d596.appspot.com",
+    messagingSenderId: "487209646537",
+    appId: "1:487209646537:web:864903f9fd9715efee9018",
+    measurementId: "G-ELXDR9YFCH"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const submitButton = document.getElementById('submit');
+
+  //BUTTON
+
+  submitButton.addEventListener('click', e => {
+    const b_email = email.value;
+    const b_password = password.value; 
+    const auth = firebase.auth();
+
+    window.alert("Infos reçues : " + b_email + "  " + b_password);
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+    promise
+      .catch(e => console.log(e.message));
+
+    });
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        console.log(firebaseUser);
+      } else {
+        console.log("not logged in");
+      }
+    })
+
+}());
