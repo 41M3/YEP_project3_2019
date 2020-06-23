6126eabb20bd307fc1d4d765a00639e8d3eecc5c
Gaëtan <gaetan.chaugny@epitech.eu>
2020-06-24 01:53:44 +0200

    [-]search.js -> transfered in newsapi.js

--- Web/public/js/newsapi.js
+++ Web/public/js/newsapi.js
@@ -35,4 +35,38 @@ async function getNews(url) {
   return articlesArray;
 }
 
+async function searchButton() {
+  const trump = document.getElementById("trump").value;
+  const searchURL = "https://newsapi.org/v2/everything?q=" + trump + "&apiKey=" + apiKey;
+  
+  let response = await fetch(searchURL);
+  let jsonResponse = await response.json();
+  let articlesArray = jsonResponse.articles;
+
+  //reset l'affichage des news
+  news.innerHTML = 0;
+  articlesArray.forEach((elemnt) => {
+      if (elemnt.author != null && elemnt.title != null && elemnt.description != null && elemnt.url != null && elemnt.urlToImage != null) {    
+        let articleRow =
+          '<div class="articlerow">' +
+          ' <div class="article">' +
+          '   <h2 class="title">' + elemnt.title + '</h2>' +
+          '   <h3>By ' + elemnt.author +'</h3>' +
+          '   <p> ' + elemnt.description + '</p>' +
+          '   <a href="' + elemnt.url + '" target="_blank" class="readmore">Read More</a>' +
+          ' </div>' +
+          ' <div class="share">' +
+          '   <img class="storyimage" src="' + elemnt.urlToImage + '" />' +
+          ' </div>' +
+          '</div>';
+    
+        news.innerHTML += articleRow;
+      }
+    });
+  return articlesArray;
+}
+
+
+
+
 getNews(Url)
